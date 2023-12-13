127c291729e55e39cffb395fce4351a25c8938a2
AleeyuDev <aleeyuidi@gmail.com>
2023-12-13 07:31:09 +0100

    My QR Code

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { QRCodeSVG } from "qrcode.react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Scan my QR code </p>
+          <QRCodeSVG
+            value="https://aleeyudev-portfolio.netlify.app/"
+            size={260}
+            level="Q"
+          />
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
